e15946e255eb44aa04f8372235757b1bb30d56e1
Inquisit <not_a_heaven@mail.ru>
2016-07-12 13:57:48 +0200

    Added work with statement.

--- AWbranch/src/branchAW/AW.java
+++ AWbranch/src/branchAW/AW.java
@@ -1,5 +1,11 @@
 package branchAW;
 
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 public class AW 
 {
 
@@ -7,8 +13,34 @@ public class AW
 	{
 		OraConnection ora = new OraConnection();
 
+		try 
+		{
+			Statement st = ora.conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			ResultSet rs = st.executeQuery("select 1 as int from dual union select 2 as int from dual");
+			ResultSetMetaData md = rs.getMetaData();
+			int iCols = md.getColumnCount();
+			ArrayList<String> sRows = new ArrayList<String>();
+			
+			while (rs.next())
+			{
+				String sRow = "";
+				for (int i = 1; i <= iCols; ++i)
+				{
+					sRow += rs.getString(i);
+				}
+				sRows.add(sRow);
+			}
+			
+			for (String s: sRows)
+			{
+				System.out.println(s);
+			}		
+		} 
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
 		
-
 		ora.Disconnect();
 	}
 
