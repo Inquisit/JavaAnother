2c67bc8fde7766c433614762e87ffe717bfaf69f
Inquisit <not_a_heaven@mail.ru>
2016-07-12 10:35:08 +0200

    AnotherTest project

---
+++ AnotherTest/src/anotherPack/AnotherClass.java
@@ -0,0 +1,11 @@
+package anotherPack;
+
+public class AnotherClass 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("AnotherClass from anotherPack of AnotherTest.");
+	}
+
+}
