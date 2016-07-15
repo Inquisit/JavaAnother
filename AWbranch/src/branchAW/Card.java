a64f7f96f2e894fbcf0e3f4387c9360f648b83bb
Inquisit <not_a_heaven@mail.ru>
2016-07-15 10:41:01 +0200

    New class.

---
+++ AWbranch/src/branchAW/Card.java
@@ -0,0 +1,12 @@
+package branchAW;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import javax.swing.ButtonGroup;
+
+public class Card 
+{
+	ArrayList<CardField> aFields;
+	Map<String, ButtonGroup> mGroups;
+}
