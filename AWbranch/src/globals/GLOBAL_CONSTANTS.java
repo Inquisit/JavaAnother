fd631fb232d331a7ae408aec6f8359ed90b3d0a8
Inquisit <not_a_heaven@mail.ru>
2016-07-19 17:03:24 +0200

    Some small changes.

--- AWbranch/src/globals/GLOBAL_CONSTANTS.java
+++ AWbranch/src/globals/GLOBAL_CONSTANTS.java
@@ -2,7 +2,7 @@ package globals;
 
 public final class GLOBAL_CONSTANTS 
 {
-	public static final int COMBOBOX_SIZE = 10;
+	public static final int COMBOBOX_SIZE = 14;
 	public static final double SCALE = 1.7;
-	public static final String THEME = "Windows";
+	public static final String THEME = "Windows Classic";
 }
