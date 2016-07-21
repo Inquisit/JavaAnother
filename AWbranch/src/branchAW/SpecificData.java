92754295b9e512498b83a70423c08e699cb1373a
Inquisit <not_a_heaven@mail.ru>
2016-07-21 14:15:33 +0200

    Extra classes.

---
+++ AWbranch/src/branchAW/SpecificData.java
@@ -0,0 +1,26 @@
+package branchAW;
+
+public class SpecificData<FIELD_TYPES> 
+{
+	String sMainFormula;
+	String sAddFormula;
+	boolean bCalcMethod;
+	boolean bRecalc;
+	String sDisabled;
+	String sInvisible;
+	String sSelFormula;
+	boolean bService;
+	
+	public SpecificData()
+	{
+		sMainFormula = "";
+		sAddFormula = "";
+		bCalcMethod = false;
+		bRecalc = false;
+		sDisabled = "";
+		sInvisible = "";
+		sSelFormula = "";
+		bService = false;
+	}
+
+}
