e43b31707b144bcdc876ebd129aa55ad96cf4354
MyTyai <dimiv007@gmail.com>
2016-07-29 10:53:39 +0300

    Git fixed

--- DbImageViewer/src/mainPackage/RetrieveImage.java
+++ DbImageViewer/src/mainPackage/RetrieveImage.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.util.zip.*;
@@ -17,6 +18,9 @@ import org.apache.commons.io.IOUtils;
 
 public class RetrieveImage 
 {  
+	public static final int dimensionX = 640;
+	public static final int dimensionY = 480;
+	
 	public static void main(String[] args) 
 	{  
 		try
@@ -73,14 +77,15 @@ public class RetrieveImage
 			JFrame jFrame= new JFrame();
 			jFrame.setLayout(null);		
 			JLabel jl = new JLabel();
-			ImageIcon icon = new ImageIcon(img);
+			Image scaled = img.getScaledInstance(dimensionX, dimensionY, java.awt.Image.SCALE_SMOOTH);
+			ImageIcon icon = new ImageIcon(scaled);
 			jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			jFrame.setSize(icon.getIconWidth(), icon.getIconHeight());
+			jFrame.setSize(dimensionX, dimensionY);
 			jFrame.setResizable(false);
 			jFrame.setLocation(150, 150);
 			jl.setIcon(icon);
 			jl.setLocation(0, 0);
-			jl.setSize(icon.getIconWidth(), icon.getIconHeight());
+			jl.setSize(dimensionX, dimensionY);
 			jl.setVisible(true);
 			jFrame.add(jl);
 			jFrame.setVisible(true); 
